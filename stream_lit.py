b255a1647ea6fc3d6add5718672c85bec2633ed7
Jay baik <ethiansjh@gmail.com>
2025-02-11 15:05:07 +0000

    test streamlit

--- stream_lit.py
+++ stream_lit.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import json
 import requests
+import src.Space_Titanic_Preproc as sp
 
-st.header("Titanic Prediction)")
+st.header("Titanic Prediction")
+st_dataframe(sp.df)
